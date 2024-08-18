5793380af5e21746bbebe510c804a2ec5a7368f5
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-08-18 11:07:04 +0900

    急上昇チャートの反映

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { SongList } from "./components/SongList";
 import spotify from "./lib/spotify";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [popularSongs, setPopularSongs] = useState([]);
   useEffect(()=>{
-    spotify.getPopularSongs()
-  },[])
+    fetchPopularSongs();
+  },[]);
+
+  const fetchPopularSongs = async () =>{
+    setIsLoading(true)
+    const result = await spotify.getPopularSongs();
+    const popularSongs = result.items.map((item) =>{
+      return item.track;
+    });
+    setPopularSongs(popularSongs);
+    setIsLoading(false)
+  }
+
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
@@ -14,7 +28,7 @@ export default function App() {
         </header>
         <section>
           <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
-          <SongList/>
+          <SongList isLoading={isLoading} songs={popularSongs}/>
         </section>
       </main>
     </div>
